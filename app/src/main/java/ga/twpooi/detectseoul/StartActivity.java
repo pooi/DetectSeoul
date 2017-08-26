52737abbbcd3d8af7e5cb9988d89ebff99352201
pooi <ldayou@naver.com>
2017-08-27 08:23:39 +0900

    코드 수정   * 시작 페이지에 배경 이미지 영역 추가   * 시작 페이지에 Search, Detect 버튼 추가   * Detect 버튼이 눌리면 이미지 인식 페이지로 넘어가도록 기능 추가

--- app/src/main/java/ga/twpooi/detectseoul/StartActivity.java
+++ app/src/main/java/ga/twpooi/detectseoul/StartActivity.java
@@ -1,13 +1,45 @@
 package ga.twpooi.detectseoul;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
+
+    private ImageView backgroundImg;
+    private TextView tv_title;
+    private TextView tv_sub_title;
+    private Button searchBtn, detectBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        init();
+
     }
+
+    private void init(){
+
+        backgroundImg = (ImageView)findViewById(R.id.background_img);
+        tv_title = (TextView)findViewById(R.id.tv_title);
+        tv_sub_title = (TextView)findViewById(R.id.tv_sub_title);
+        searchBtn = (Button)findViewById(R.id.search_btn);
+        detectBtn = (Button)findViewById(R.id.detect_btn);
+
+        detectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
 }
