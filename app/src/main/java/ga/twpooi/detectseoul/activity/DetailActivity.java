d82990c96cbd2bdf8f269e28b126e9f3ec24d3fb
pooi <ldayou@naver.com>
2017-09-14 21:06:16 +0900

    기능 추가   * 지도에 명소의 위치에 마크를 표시하도록 함

--- app/src/main/java/ga/twpooi/detectseoul/activity/DetailActivity.java
+++ app/src/main/java/ga/twpooi/detectseoul/activity/DetailActivity.java
@@ -30,6 +30,7 @@ import com.nineoldandroids.view.ViewHelper;
 import com.nineoldandroids.view.ViewPropertyAnimator;
 import com.squareup.picasso.Picasso;
 
+import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
@@ -226,7 +227,16 @@ public class DetailActivity extends BaseActivity implements ObservableScrollView
                     }
                     return false;
                 }
+
             });
+            MapPOIItem marker = new MapPOIItem();
+            marker.setItemName(attraction.title);
+            marker.setTag(0);
+            marker.setMapPoint(MapPoint.mapPointWithGeoCoord(attraction.lat, attraction.lng));
+            marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+            marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+
+            mapView.addPOIItem(marker);
             mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(attraction.lat, attraction.lng), true);
 
             ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
