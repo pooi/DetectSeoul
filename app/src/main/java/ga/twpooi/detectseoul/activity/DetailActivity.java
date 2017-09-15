4f016c7af3841cacf5b430795e46caf0f7fce8a0
pooi <ldayou@naver.com>
2017-09-15 09:25:48 +0900

    기능 추가   * 명소 소개 웹페이지가 있을 경우 웹페이지를 보여주고 터치시 웹페이지로 이동할 수 있도록 함

--- app/src/main/java/ga/twpooi/detectseoul/activity/DetailActivity.java
+++ app/src/main/java/ga/twpooi/detectseoul/activity/DetailActivity.java
@@ -263,6 +263,27 @@ public class DetailActivity extends BaseActivity implements ObservableScrollView
             li_detail.addView(v);
         }
 
+        if(attraction.web != null && !"".equals(attraction.web)){
+
+            View v = getLayoutInflater().inflate(R.layout.detail_information_custom_item, null, false);
+            TextView tv_title = (TextView)v.findViewById(R.id.tv_title);
+            TextView tv_content = (TextView)v.findViewById(R.id.tv_content);
+
+            tv_title.setText("웹페이지");
+            tv_content.setText(attraction.web);
+            tv_content.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+//                Toast.makeText(DetailActivity.this, "FAB is clicked", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse(attraction.web));
+                    startActivity(intent);
+                }
+            });
+
+            li_detail.addView(v);
+
+        }
+
         for(String[] strs : attraction.detail){
 
             View v = getLayoutInflater().inflate(R.layout.detail_information_custom_item, null, false);
